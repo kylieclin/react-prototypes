785205bc6929d4ec878001d0809674f2339cfdae
Kylie Lin <kylieclin@gmail.com>
2019-03-26 17:39:37 -0700

    react intro prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const data = {
+    user: 'Dan',
+    number: luckyNumber()
+}
+
+function luckyNumber(){
+    const number = Math.floor((Math.random()*1001)+1);
+    return number;
+}
+
+function greeting (data){
+    return(<div>
+        <h1>Welcom {data.user}</h1>
+        <h2 className="text-muted">Your lucky number is: {data.number}</h2>
+    </div>)
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(data),
     document.getElementById('root')
 );

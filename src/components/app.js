61ed98ba3d23dc5ed9c706a8f926eb0c5153f457
Kylie Lin <kylieclin@gmail.com>
2019-03-26 18:09:51 -0700

    funcational component practice

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import Table from './table';
+
+const students =[{name: 'Dan', course:'Java', grade: 100},{name: 'Vivi', course:'Cook', grade: 100},{name: 'Jo', course:'PE', grade: 100}];
+
+
+export default ()=>{
+    return (<div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+    </div>)
+}
 
 
